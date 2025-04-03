61a9efbc0f5549937df644c761e281e0772c0f4c
Wenqing Zong <wenqing.zong98@gmail.com>
2025-04-03 02:16:37 +0100

    Add react compiled file

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,9 @@
 #[macro_use]
 extern crate rocket;
 
-#[cfg(test)]
-mod tests;
-
-use include_dir::{include_dir, Dir, DirEntry};
-use rocket::form::Form;
-use rocket::fs::{relative, FileServer, NamedFile};
+use include_dir::{include_dir, Dir};
 use rocket::http::ContentType;
-use rocket::response::stream::{Event, EventStream};
-use rocket::serde::{Deserialize, Serialize};
-use rocket::tokio::select;
-use rocket::tokio::sync::broadcast::{channel, error::RecvError, Sender};
-use rocket::{Shutdown, State};
 use std::path::{Path, PathBuf};
-
-#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
-#[cfg_attr(test, derive(PartialEq, UriDisplayQuery))]
-#[serde(crate = "rocket::serde")]
-struct Message {
-    #[field(validate = len(..30))]
-    pub room: String,
-    #[field(validate = len(..20))]
-    pub username: String,
-    pub message: String,
-}
-
 static STATIC_DIR: Dir = include_dir!("static");
 
 /// Returns any requested files.
@@ -48,37 +26,7 @@ fn files(path: PathBuf) -> Option<(ContentType, &'static [u8])> {
     Some((content_type, file.contents()))
 }
 
-/// Returns an infinite stream of server-sent events. Each event is a message
-/// pulled from a broadcast queue sent by the `post` handler.
-#[get("/events")]
-async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
-    let mut rx = queue.subscribe();
-    EventStream! {
-        loop {
-            let msg = select! {
-                msg = rx.recv() => match msg {
-                    Ok(msg) => msg,
-                    Err(RecvError::Closed) => break,
-                    Err(RecvError::Lagged(_)) => continue,
-                },
-                _ = &mut end => break,
-            };
-
-            yield Event::json(&msg);
-        }
-    }
-}
-
-/// Receive a message from a form submission and broadcast it to any receivers.
-#[post("/message", data = "<form>")]
-fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
-    // A send 'fails' if there are no active subscribers. That's okay.
-    let _res = queue.send(form.into_inner());
-}
-
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .manage(channel::<Message>(1024).0)
-        .mount("/", routes![post, events, files])
+    rocket::build().mount("/", routes![files])
 }
